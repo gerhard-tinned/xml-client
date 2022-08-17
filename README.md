b98171c069699adbcdb557d00334a32effa8890c
Gerhard Steinbeis <Gerhard.Steinbeis2@s-itsolutions.at>
2022-08-17 11:03:56 +0200

    xml-client and soap example requests for NicTool

---
+++ README.md
@@ -0,0 +1,111 @@
+# The XML client
+
+The XML client is a very simple shell script implementation allowing to interact with a XML API. 
+
+The script allows to define a XML template for each request available via the API. Additionally to the template, a configuration file allows to set parameters for the request. 
+
+## Requirements
+
+This script uses internally the following tools which must be installed. 
+
+* jq
+
+Besides the Linux core utilities, the script requires "jq" to be installed which is used to parse the json response from the API.
+
+
+## Configuration
+
+A generic configuration file is available in the template directory called "_api.conf". It contains the URL to the API server.
+
+```
+XML_URL=http://127.0.0.1:18082/soap
+```
+
+## XML template
+
+The XML template contains the XML that will be sent in the requests to the API. All parameters should be replaced with placeholders of the format "#PARAMETERNAME#" (The parameter-name surrounded by '#'). Each line must only contain one parameter placeholder. 
+
+The placeholder name is also the name used as the variable-name to set the value for that parameter. Lines with parameters which are not defined in the request will be removed. 
+
+
+## CFG files
+
+The config files contain configuration elements for the request.
+
+```
+RESULT_FIELDS="nt_user_id"
+OK_FIELD_CHECK="nt_user_id"
+ERROR_FIELDS="error_code error_desc error_msg"
+
+```
+
+* The RESULT_FIELDS is a space separated list of field names in the XML returned in a success response.
+* The OK_FIELD_CHECK is a single field name found in successful a response. If the field is found in the response, the request is considered successful.
+* The ERROR_FIELDS is a space separated list of field names in the XML returned in a non-success response. The list of fields is returned when the request was not successful (OK_FIELD_CHECK field not returned).
+
+The success of a request is identified by the presence of the field name specified in the OK_FIELD_CHECK configuration.
+
+## Calling the script 
+
+```
+$ ./xml-client.sh -h
+
+Usage: xml-client.sh [-hvd] --tpath /path/to/tmplates/ --request NAME --list VARIABLES
+  -h  --help              print this usage and exit
+  -v  --version           print version information and exit
+  -d                      Enable the debug output
+  -t  --tpath DIR         Directory containing the request templates
+                          default is the script path subdirectory xml_tmpl
+  -r  --request NAME      The name or the request to perform
+      --list              List the templates request variables and response variables
+                          When given without a request name, the available requests are listed
+
+VARIABLES can be set as environment variables or as arguments in the followiug format
+      NAME value          The variables can be passed to the script as arguments
+```
+
+In usual XML APIs the first request needed is the authentication request. It is suggested to define the RESULT_FIELDS in a way that it can be used in the requests after. 
+
+```
+$ ./xml-client.sh -r login USERNAME root PASSWORD 'Pa55wordUser1'
+NT_USER_SESSION 62fbca01a3e613f5
+```
+
+When configured in the correct way (like shown above), the resulting field will be the session token required for the requests after the authentication. Sometimes it can be enough to store the output on a variable to be used in following requests.
+
+
+```
+SESSION_TOKEN=$(./xml-client.sh -r login USERNAME root PASSWORD 'Pa55wordUser1')
+```
+
+
+## Showing Request Information
+
+The script allows to list the available request using the "--list" option. 
+
+```
+$ ./xml-client.sh --list
+login
+new_user
+verify_session
+new_nameserver
+get_nameserver
+delete_nameserver
+get_group_groups
+```
+
+Combined with the "-r" option and a request name with the "--list" option, the list of variables in the template are listed as well as the returned Fields from the response.
+
+```
+$ ./xml-client.sh --list -r login
+
+# login
+
+## Request Variables
+* USERNAME
+* PASSWORD
+
+## Response Variables
+* NT_USER_SESSION
+```
+
---
+++ xml-client.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+# 
+VERSION="0.1.1"
+#
+
+#
+# Help screen
+#
+function help_screen () {
+    echo 
+    echo "Usage: $(basename $0) [-hvd] --tpath /path/to/tmplates/ --request NAME --list VARIABLES"
+    echo "  -h  --help              print this usage and exit"
+    echo "  -v  --version           print version information and exit"
+    echo "  -d                      Enable the debug output"
+    echo "  -t  --tpath DIR         Directory containing the request templates "
+    echo "                          default is the script path subdirectory xml_tmpl"
+    echo "  -r  --request NAME      The name or the request to perform"
+    echo "      --list              List the templates request variables and response variables"
+    echo "                          When given without a request name, the available requests are listed"
+    echo ""
+    echo "VARIABLES can be set as environment variables or as arguments in the followiug format"
+    echo "      NAME value          The variables can be passed to the script as arguments "
+}
+
+
+
+function errorout () {
+    echo -e "$@" >&2
+}
+
+function echoout () {
+    echo -e "$@"
+}
+
+function formatout () {
+    FORMATOUT_FORMAT=$1
+    shift
+    printf "${FORMATOUT_FORMAT}" $@
+}
+
+function debugout () {
+    # Send debug output when debug mode is enabled
+    if [[ ${DEBUG} != 0 ]]; then
+        echo -e "$@" >&2
+    fi
+}
+
+OSTYPE=$(uname -s)
+
+if [[ "${OSTYPE}" == "Darwin" ]]; then
+    TEMPLATE_PATH="$(dirname $0)/xml_tmpl"
+else
+    TEMPLATE_PATH="$(dirname $(readlink -f $0))/xml_tmpl"
+fi
+
+DEBUG=0
+while [ $# -gt 0 ]; do
+    case $1 in
+        # General parameter
+        -h|--help)
+            help_screen
+            exit 0
+            ;;
+
+        -v|--version)
+            echo 
+            echo "`basename $0` version ${VERSION}"
+            echo
+            exit 0
+            ;;
+
+        -d)
+            DEBUG=1
+            shift 1
+            ;;
+
+        -t|--tpath)
+            if [[ ! -z "$2" ]] && [[ -d "$2" ]]; then
+                TEMPLATE_PATH=$2
+            else
+                echo "ERROR: Specified template directory does not exist or is not a directory."
+                help_screen
+                exit 1
+            fi
+            shift 2
+            ;;
+
+        -r|--request)
+            if [[ ! -z "$2" ]] && [[ -f "${TEMPLATE_PATH}/$2.xml" ]]; then
+                REQUEST_NAME=$2
+            else
+                echo "ERROR: Specified template directory does not exist or is not a directory."
+                help_screen
+                exit 1
+            fi
+            shift 2
+            ;;
+
+        -l|--list)
+            COMMAND="LIST"
+            shift 1
+            ;;
+
+        # VARIABLES        
+        *)
+            if [[ "${1}" == "$(echo ${1} | egrep '^[A-Z_]+$')" ]] && [[ ! -z "$2" ]]; then
+                export XMLSV_${1}=${2}
+                shift 2
+            else
+                echo "ERROR: Unknown option '$1'"
+                help_screen
+                exit 1
+                break
+            fi
+            ;;
+    esac
+done
+
+
+# load the PAI configuration
+. ${TEMPLATE_PATH}/_api.conf
+
+
+#
+# List available requests
+#
+if [[ "${COMMAND}" == "LIST" ]] && [[ -z "${REQUEST_NAME}" ]]; then
+    find xml_tmpl/ -name *.xml | sed -Ee 's|^.*/([a-z_]+)\.xml|\1|'
+    exit 0
+fi
+
+
+#
+# List Request variables and response variables
+#
+if [[ "${COMMAND}" == "LIST" ]]; then
+    if [[ -f "${TEMPLATE_PATH}/${REQUEST_NAME}.xml" ]]; then
+        FIELDS=$(egrep '#[A-Z_]+#' ${TEMPLATE_PATH}/${REQUEST_NAME}.xml | sed -Ee 's/^.*#([A-Z_]+)#.*$/\1/')
+        . ${TEMPLATE_PATH}/${REQUEST_NAME}.cfg
+
+        echoout ""
+        echoout "# ${REQUEST_NAME}"
+        echoout ""
+        echoout "## Request Variables"
+        for F in ${FIELDS}; do
+            echoout "* ${F}"
+        done
+        echoout ""
+        echoout "## Response Variables"
+        for F in ${RESULT_FIELDS}; do
+            echoout "* $(echo ${F} | tr '[:lower:]' '[:upper:]') "
+        done
+        echoout ""
+    else 
+        errorout "Unknown request '${REQUEST_NAME}'."
+        exit 1
+    fi
+    exit 0
+fi
+
+# Reading files
+if [[ -f "${TEMPLATE_PATH}/${REQUEST_NAME}.xml" ]]; then
+    FIELDS=$(egrep '#[A-Z_]+#' ${TEMPLATE_PATH}/${REQUEST_NAME}.xml | sed -Ee 's/^.*#([A-Z_]+)#.*$/\1/')
+    . ${TEMPLATE_PATH}/${REQUEST_NAME}.cfg
+    TMP_FILE=$(umask 077 && mktemp /tmp/xml.XXXXXXXXXX)
+else 
+    errorout "Request template could not be found."
+    exit 1
+fi
+
+# Reading VARIABLES from the request file
+FIELD_REPLACE=""
+for F in ${FIELDS}; do
+    SF="XMLSV_${F}"
+    if [[ ! -z "${!SF}" ]]; then
+        FIELD_REPLACE="${FIELD_REPLACE} -e s|#${F}#|${!SF}| "
+    fi
+done
+
+# Replacing the variables with content ; Removing elements with NO variables value
+sed ${FIELD_REPLACE} ${TEMPLATE_PATH}/${REQUEST_NAME}.xml | egrep -v "#[A-Z_]+#" >${TMP_FILE}
+
+
+if [[ "${DEBUG}" == "1" ]]; then
+    echo "## REQUEST XML"
+    cat ${TMP_FILE} 
+    echo -e "\n"
+fi
+
+# Send XML request
+RESPONSE_RAW=$(curl  --header "Content-Type: text/xml;charset=UTF-8" --data @${TMP_FILE} ${XML_URL} 2>/dev/null)
+if [[ "${DEBUG}" == "1" ]]; then
+    echo "## RESPONSE_RAW"
+    echo "${RESPONSE_RAW}" | xmllint --format -
+    echo -e "\n"
+fi
+
+
+
+# Check the response
+OK_CHECK=$(echo ${RESPONSE_RAW} | grep "<\/${OK_FIELD_CHECK}>")
+if [[ -z "${OK_CHECK}" ]]; then
+    debugout "*** ERROR ***"
+    for EF in ${ERROR_FIELDS}; do
+        EF_VALUE=$(echo ${RESPONSE_RAW} | grep "<\/${EF}>" | sed -E "s/^.*<${EF}.*>(.*)<\/${EF}>.*$/\1/")
+        echoout "$(echo ${EF} | tr '[:lower:]' '[:upper:]') ${EF_VALUE}"
+    done
+else
+    debugout "*** SUCCESS ***"
+    for RF in ${RESULT_FIELDS}; do
+        RF_VALUE=$(echo ${RESPONSE_RAW} | grep "<\/${RF}>" | sed -E "s/^.*<${RF}.*>(.*)<\/${RF}>.*$/\1/")
+        echoout "$(echo ${RF} | tr '[:lower:]' '[:upper:]') ${RF_VALUE}"
+    done
+fi
+echo ""
+
+
+rm ${TMP_FILE}
